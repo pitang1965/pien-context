df14db54f007d447668a23dd07db1b9d52a89df5
Shoji Makino <pitang1965@gmail.com>
2022-12-21 12:07:02 +0900

    💄 Add text to About page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Layout } from '@components/layouts/Layout';
-import { Container, Title } from '@mantine/core';
+import { Container, Text, Title } from '@mantine/core';
 
 export const AboutPage = () => {
   return (
     <Layout content='About'>
       <Container>
         <Title>About</Title>
+        <Text>状態管理にReactのContext APIを使用したぴえんです。</Text>
       </Container>
     </Layout>
   );
